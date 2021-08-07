12aa7d7f07dccc20f726816c8484e6097477f6dd
Mohammed Fetouah <Fetouah.m@gmail.com>
2021-08-07 22:56:21 +0200

    page index bientot finis coté js

--- src/index.js
+++ src/index.js
@@ -8,30 +8,65 @@ class Produit {
         this.colors = colors;
     }
     getHtml () {
-        var newDiv = document.createElement("div");
-        var newImg = document.createElement("img");
-        newImg.setAttribute("src",this.imageUrl);
-        newImg.setAttribute("alt","image de " + this.name);
-        newDiv.appendChild(newImg);
-        return newDiv;
+        return         '<a href="produit.html?id=' + this._id +  '" class="produits"> ' +
+                            '<div>' +
+                                '<img class="produit-image"' + ' src="'  + this.imageUrl + '" alt="nounours">' +                             
+                            '</div>' +
+                            '<div class="produit-description">' +
+                                '<div class="produit-name">' +
+                                    '<span>' + this.name + '</span>' +
+                                '</div>' +
+                                ' <div class="produit-price"> ' + 
+                                    ' <span> ' + this.price + '</span>' +
+                                '</div>' + 
+                            '</div>' +
+                        '</a>';
+                        
+
+
+
+
+
+
+        // var newA = document.createElement("a");
+        // var newDivImg = document.createElement('div');
+        // var newImg = document.createElement("img");
+        // var newDivDescription = document.createElement("div");
+        
+        // newImg.setAttribute("src",this.imageUrl);
+        // newImg.setAttribute("alt","image de " + this.name);
+        // newA.setAttribute('href', 'produit.html?id= ' + this._id);
+        // newDivImg.appendChild(newImg);
+        // newA.appendChild(newDivImg);
+        // return newA;
 
     }
 }
-fetch('http://localhost:3000/api/teddies')
-.then(function(res) {
-if (res.ok) {
-    return res.json();
-}
-})
-.then(function(data) {
-    for(produitData of data) {
-        var produit = new Produit(produitData._id, produitData.price, produitData.name, produitData.imageUrl, produitData.description, produitData.colors);
-        var currentDiv = document.getElementById('container');
-        currentDiv.appendChild(produit.getHtml());
+
+
+if(window.location.pathname == '/') {
+    fetch('http://localhost:3000/api/teddies')
+    .then(function(res) {
+    if (res.ok) {
+        return res.json();
     }
-})
-.catch(function(err) {
-});
+    })
+    .then(function(data) {
+        for(produitData of data) {
+            var produit = new Produit(produitData._id, produitData.price, produitData.name, produitData.imageUrl, produitData.description, produitData.colors);
+            var currentDiv = document.getElementById('container');
+            currentDiv.innerHTML += produit.getHtml();
+            console.log(produit.getHtml());
+        }
+    })
+    .catch(function(err) {
+    });
+}
+if(window.location.pathname == "/produit.html") {
+    // fetch('http://localhost:3000/api/teddies/') recuperer parametre d'url
+
+}
+
 
 
 
