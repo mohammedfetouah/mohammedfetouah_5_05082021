06cec0567637d2f9af46fe77ba4c4e2f077e0958
Mohammed Fetouah <Fetouah.m@gmail.com>
2021-08-22 20:28:29 +0200

    revoir le panier

--- src/index.js
+++ src/index.js
@@ -43,85 +43,70 @@ class Produit {
                                 ' <div class="produit-price"> ' + 
                                     ' <span> ' + this.price/100 + '€</span>' +
                                 '</div>' + 
-                                '<select name="colors">' + colorHtml + '</select>' + 
-                                '<button id="addPanier">  ajouter au panier </button>' +
-                            '</div>';
+                                '<select name="colors" id="addColors">' + colorHtml + '</select>' + 
+                            '</div>' +
+                            '<button id="addPanier">  ajouter au panier </button>' +
+                        '</div>';
     }  
-    getHtmlQuantity() {
-        return                  '<select name="quantite" id="addQuantity">' +
-                                    '<option value="1">1</option>' +
-                                    '<option value="2">2</option>' + 
-                                    '<option value="3">3</option>' +
-                                    '<option value="4">4</option>' +
-                                    '<option value="5">5</option>' +
-                                    '<option value="6">6</option>' +
-                                    '<option value="7">7</option>' +
-                                    '<option value="8">8</option>' +
-                                    '<option value="9">9</option>' +
-                                    '<option value="10">10</option>' +
-                                '</select>';
-
-    }
     
 }
-
+// 5beaa8bf1c9d440000a57d94_white: {
+//     name: 'Arnold',
+//     qty: 1,
+//     price: 5000
+// },
 class Panier {
-    addProduct(productId, productPrice, productColors) {
-
-        var panierColors = localStorage.getItem('colors')
-        if(panierColors) {
-            panierColors = JSON.parse(panierColors)
-        } else {
-            panierColors = [];
-        }
-        panierColors.push(productColors);
-        localStorage.setItem('colors', JSON.stringify(panierColors))
-
-
-
+    addProduct(productId, productName, productPrice, productColor) {
+        var productColorKey = productColor.toLowerCase().replace(' ', '_');
+        var productKey = productId + '_' + productColorKey;
+        console.log(productKey);
         
         var panierLocal = localStorage.getItem('panier');
         if(panierLocal) {
             panierLocal = JSON.parse(panierLocal);
         }else {
-            panierLocal = [];
+            panierLocal = {};
         }
-        for( var i = 1; i < quantity; i++) {
-            panierLocal.push(productId)
-        }
-        panierLocal.push(productId);
+        panierLocal[productKey] = {
+            name : productName,
+            qty : 1,
+            color : productColor,
+            price : productPrice,
+        };
+        
         localStorage.setItem('panier', JSON.stringify(panierLocal));
-
-        
-
-        var panierTotal = localStorage.getItem('panierTotal');
-        if(panierTotal) {
-            panierTotal = parseInt(panierTotal);
-            panierTotal += parseInt(productPrice)*quantity;
-            localStorage.setItem('panierTotal', panierTotal);
-        
-        } else {
-            localStorage.setItem('panierTotal', productPrice*quantity);
-        }
-
-
-
-
     }
     getProduits() {
         var panierLocal = localStorage.getItem('panier');
         return JSON.parse(panierLocal);
     }
     getTotal() {
-        return localStorage.getItem('panierTotal');
+        var total = 0;
+        for (let [key, value] of Object.entries(this.getProduits())) {
+            total += value.price*value.qty;
+        }
+        return total;
     }
-    getQuantity() {
-        return localStorage.getItem('quantite');
+    getColors() {
+        return localStorage.getItem('colors');
+    }
+    getHtmlQuantity(productKey, qty) {
+        var html = '<select name="quantite" id="' + productKey + '">';
+        for (var i = 1;i<=10;i++) {
+            if (i == qty) {
+                html +=  '<option value="'+ i + '" selected>' + i + '</option>';
+            } else {
+                html +=  '<option value="'+ i + '">' + i + '</option>';
+            }
+            
+        }
+        html += '</select>';
+
+        return html;
     }
 
 }
 var panier = new Panier();
-
 var commande = new Commande();
 
 class Commande {
@@ -236,11 +221,17 @@ if(window.location.pathname == "/produit.html") {
 
         var addPanier = document.getElementById('addPanier');
 
-        addPanier.addEventListener('click', function (e) {
-            panier.addProduct(data._id, data.price, data.colors);
 
-            window.location.href = "http://localhost:8080/";
-            window.alert("l'article a bien était ajoutée au panier !");
+
+        addPanier.addEventListener('click', function (e) {
+            var color = document.getElementById('addColors').value;
+            panier.addProduct(data._id,data.name, data.price, color);
+
+
+
+
+            
+
            
         });
     })
@@ -250,67 +241,26 @@ if(window.location.pathname == "/produit.html") {
 }
 
 
-
-
-
-
-
-// if(window.location.pathname == "/panier.html") {
+if(window.location.pathname == "/panier.html") {
     
-//     var clearPanier = document.getElementById('clearPanier');
-//     clearPanier.addEventListener('click', function(){
-//         localStorage.clear();
-//         window.location.href = "http://localhost:8080/panier.html";
-//     });
-//     if (panier.getProduits()) {
-        
-//         document.getElementById('total').innerHTML += '<div><b>Total panier : </b>' + (panier.getTotal()/100) + '€</div>';
-//         var productQty = {};
-//         var productIds = [];
-//         for(productId of panier.getProduits()) {
-//             if (productQty[productId]) {
-//                 productQty[productId] += 1;
-//             } else {
-//                 productQty[productId] = 1;
-//                 productIds.push(productId);
-//             }
-//         }
-//         for(productId of productIds) {
-  
-//             fetch('http://localhost:3000/api/teddies/' + productId)
-//             .then(function(res) {
-//                 if (res.ok) {
-//                     return res.json();
-//                 } else {
-//                     window.location.href = "http://localhost:8080/";
-//                 }
-//             })
-//             .then(function(data) {
-//                 var currentDiv = document.getElementById('produits');
-//                 // currentDiv.innerHTML += 
-                                                           
-//                                             // '<span class="qty"> '+ productQty[data._id] +' </span>' +  
-                                        
-                                        
-//             })
-//             .catch(function(err) {
-//             });
-//         }
-        
+    var clearPanier = document.getElementById('clearPanier');
+    clearPanier.addEventListener('click', function(){
+        localStorage.clear();
+        window.location.href = "http://localhost:8080/panier.html";
+    });
+    for (let [key, value] of Object.entries(panier.getProduits())) {
+        // Ajouté ligne dans le panier avec  Nom (couleur) selectQty et prix
+        // ajouter eventListener change select 
+    }
 
+    var total = document.getElementById('total').innerHTML = '<div><b>Total:</b>' +  panier.getTotal()/100  + '€</div>' ; 
+    document.getElementById('cordonnees').addEventListener('submit', function(event) {
+        event.preventDefault();
+        commande.addCommande()
+        window.location.href = "http://localhost:8080/commande.html";
+    })
 
-//     }
-//     document.getElementById('cordonnees').addEventListener('submit', function(event) {
-
-//         event.preventDefault();
-//         commande.addCommande()
-   
-
-
-//         window.location.href = "http://localhost:8080/commande.html";
-//     })
-
-// }
+}
 
 
 
